f8c5ac925b04c09843874e126400589e7b500939
sangjin-hash <lsj9873@ajou.ac.kr>
2021-12-31 22:38:50 +0900

    DataBinding & Navigation Tutorial Completed

--- step3/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ step3/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,23 +1,13 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TitleFragment : Fragment() {
 
     override fun onCreateView(
@@ -29,6 +19,18 @@ class TitleFragment : Fragment() {
         //false -> attachToParent Value
         //binding.root -> From the method, which contains the inflated view
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener{v->v.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)}
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
